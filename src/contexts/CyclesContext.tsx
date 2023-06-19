2a28345a631a1c1bdf45721da1a79864eb9506cd
francisco.barbosa <francisco.barbosa@mv.com.br>
2023-06-19 20:25:21 -0300

    using immer

--- src/contexts/CyclesContext.tsx
+++ src/contexts/CyclesContext.tsx
@@ -64,7 +64,7 @@ export function CyclesContextProvider({
   }
 
   function interruptCurrentCycle() {
-    dispatch(interruptCurrentCycleAction)
+    dispatch(interruptCurrentCycleAction())
   }
 
   return (
